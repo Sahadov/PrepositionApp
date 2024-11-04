f8dc79647fe5b2e367db2e81134c6e4d6afff10d
Dmitry <mrsahadov@gmail.com>
2024-11-04 13:31:45 -0300

    Add project

---
+++ PrepositionGuru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrepositionGuru
+//
+//  Created by Дмитрий Волков on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
